5bed4acdce15b1b6d1429d725c182ccb6b040311
Katia610 <93476065+Katia610@users.noreply.github.com>
2022-08-16 16:25:54 +0100

    Styling of the app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,33 @@ function App() {
       <div className="container">
         <header className="App-header">Dictionary</header>
         <main>
-          <Dictionary defaultKeyword="hello" />
+          <Dictionary defaultKeyword="yoga" />
         </main>
-        <br />
+
         <footer>
-          Coded by Catherine Razafindralambo and hosted on Netlify
+          <a
+            href="https://github.com/Katia610/dictionary-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Coded{" "}
+          </a>
+          by{" "}
+          <a
+            href="https://uk.linkedin.com/in/catherine-razafindralambo-7832b127"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Catherine Razafindralambo
+          </a>{" "}
+          and
+          <a
+            href="https://tangerine-basbousa-2e39fd.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""} hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
