095482c5b90bf4e11022884d0b2af98a787f6d27
Katia610 <93476065+Katia610@users.noreply.github.com>
2022-08-17 12:22:22 +0100

    Cleared the warnings of the console

--- src/Photos.js
+++ src/Photos.js
@@ -10,11 +10,7 @@ export default function Photos(props) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  <img
-                    src={photo.src.landscape}
-                    alt="photo representing the searched word"
-                    className="img-fluid"
-                  />
+                  <img src={photo.src.landscape} className="img-fluid" alt="" />
                 </a>
               </div>
             );
